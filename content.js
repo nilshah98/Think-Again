879300f07005dd861457dcf36454e03b6130fb18
nilshah98 <neel12@somaiya.edu>
2019-12-07 02:14:00 +0530

    CSS separated

--- content.js
+++ content.js
@@ -1,29 +1,17 @@
 // Creating faded element
 const fadeDiv = document.createElement("div");
-fadeDiv.style.position = "fixed";
-fadeDiv.style.top = "0";
-fadeDiv.style.left = "0";
-fadeDiv.style.bottom = "0";
-fadeDiv.style.right = "0";
-fadeDiv.style.background = "rgba(50,50,50,0.5)";
-fadeDiv.style.zIndex = "99999";
+fadeDiv.classList.add("fadeDiv");
 
 // Creating Message Div
 const messageDiv = document.createElement("div");
-messageDiv.style.position = "absolute";
-messageDiv.style.width = "50%";
-messageDiv.style.height = "30%";
-messageDiv.style.top = "50%";
-messageDiv.style.left = "50%";
-messageDiv.style.transform = "translate(-50%, -50%)";
-messageDiv.style.background = "#fff";
-messageDiv.textContent = "Think Again";
+messageDiv.classList.add("messageDiv");
 
 // Appending message div to fade element
 fadeDiv.appendChild(messageDiv)
 
 // Selecting body element
 const body = document.getElementsByTagName("body")[0];
+const html = document.getElementsByTagName("html")[0];
 
 // *************************************************** //
 // *******************MAIN CONTENT******************** //
@@ -32,6 +20,6 @@ const body = document.getElementsByTagName("body")[0];
 chrome.runtime.onMessage.addListener((message) => {
     console.log(`Message Received - ${message.url}`);
     messageDiv.textContent = message.url;
-    // body.append(fadeDiv);
-    // body.style.overflow = "hidden";
+    body.classList.add("noScroll");
+    html.appendChild(fadeDiv);
 })
