61f4abfacfda4ec7fda77c0f837e527d92a7e266
nilshah98 <neel12@somaiya.edu>
2019-12-08 17:24:00 +0530

    UI fixes

--- content.js
+++ content.js
@@ -12,17 +12,31 @@ messageDiv.classList.add("messageDiv");
 const carryOn = document.createElement("div");
 carryOn.classList.add("button");
 carryOn.classList.add("carryOn");
+// carryOn.classList.add("blocked");
 carryOn.textContent = "carry on...";
 
 // Create get me out of here
 const getMeOut = document.createElement("div");
 getMeOut.classList.add("button");
 getMeOut.classList.add("getMeOut");
+// getMeOut.classList.add("blocked");
 getMeOut.textContent = "get me out!";
 
+// creating messageButton
+const messageButtons = document.createElement("div");
+messageButtons.classList.add("messageButtons");
+// messageButtons.classList.add("noCursor");
+
+// creating messageText
+const messageText = document.createElement("div");
+messageText.classList.add("messageList");
+
 // Appending message div to fade element
+messageButtons.appendChild(carryOn);
+messageButtons.appendChild(getMeOut);
+messageDiv.appendChild(messageText);
+messageDiv.appendChild(messageButtons);
 fadeDiv.appendChild(messageDiv);
-console.log("Appended buttons and message div");
 
 // Selecting body element
 // const body = document.getElementsByTagName("body")[0];
@@ -44,9 +58,13 @@ getMeOut.addEventListener("click",() => {
 chrome.runtime.onMessage.addListener((message,_sender,_sendResponse) => {
     console.log("message received");
     console.log(message);
-    messageDiv.textContent = `You've opened ${message.domain} for ${message.timesOpened} times today and used ${(message.timeSpent)/1000} seconds today`;
-    messageDiv.appendChild(carryOn);
-    messageDiv.appendChild(getMeOut);
+    messageText.textContent = `You've opened 👀 ${message.domain} for 🔓 ${message.timesOpened} times today and used ⏳ ${((message.timeSpent)/60000).toFixed(2)} minutes today`;
     html.classList.add("noScroll");
     html.appendChild(fadeDiv);
+
+    // setTimeout(() => {
+    //     carryOn.classList.remove("blocked");
+    //     getMeOut.classList.remove("blocked");
+    //     messageButtons.classList.remove("noCursor");
+    // }, 5000);
 })
